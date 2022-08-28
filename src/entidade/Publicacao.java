4eb899af1b8b71128f773f995c062dd217185b5d
ChristianoCastro <111785366+ChristianoCastro@users.noreply.github.com>
2022-08-28 13:58:44 -0300

    Add files via upload

---
+++ src/entidade/Publicacao.java
@@ -0,0 +1,84 @@
+package entidade;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+public class Publicacao {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+	private Date momento;
+	private String titulo;
+	private String conteudo;
+	private Integer likes;
+	
+	private List<Comentario> comentarios = new ArrayList<>();
+	
+	public Publicacao() {
+	}
+
+	public Publicacao(Date momento, String titulo, String conteudo, Integer likes) {
+		this.momento = momento;
+		this.titulo = titulo;
+		this.conteudo = conteudo;
+		this.likes = likes;
+	}
+
+	public Date getMomento() {
+		return momento;
+	}
+
+	public void setMomento(Date momento) {
+		this.momento = momento;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public List<Comentario> getComentario() {
+		return comentarios;
+	}
+
+	public void addComentario(Comentario comment) {
+		comentarios.add(comment);
+	}
+
+	public void removeComentario(Comentario comment) {
+		comentarios.remove(comment);
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(titulo + "\n");
+		sb.append(likes);
+		sb.append(" Likes - ");
+		sb.append(sdf.format(momento) + "\n");
+		sb.append(conteudo + "\n");
+		sb.append("Comentarios:\n");
+		for (Comentario c : comentarios) {
+			sb.append(c.getTexto() + "\n");
+		}
+		return sb.toString();
+	}
+}
